e52b8c393ee5deaa956818ff5e5e065ffeb2c137
Mansur <mustafin.mansur02@gmail.com>
2024-04-11 12:23:36 +0100

    Added game class
    Co-authored-by: Daniel Ferreira <dsantosferreira@users.noreply.github.com>
    Co-authored-by: Francisco Cardoso <franciscocardoso.3003@gmail.com>

---
+++ assign2/src/Nim.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Nim {
+    private int coins;
+    private int currentPlayer;
+    private int nPlayers;
+
+    public Nim(int startingCoins, int nPlayers) {
+        Random random = new Random();
+        this.nPlayers = nPlayers;
+        int lowerBound = this.nPlayers * 8 - 7;
+        int upperBound = this.nPlayers * 8 + 7;
+        this.coins = lowerBound + random.nextInt(upperBound - lowerBound + 1);
+        this.currentPlayer = 0;
+    }
+
+    // Method to make a move
+    public int move(int coinsTaken) {
+        if (coinsTaken < 1 || coinsTaken > 2) {
+            return -1; // TODO enums
+        }
+        if (coinsTaken > coins) {
+            return -2;
+        }
+
+        // Update the state
+        coins -= coinsTaken;
+
+        
+        currentPlayer = (currentPlayer + 1) % nPlayers;
+        return getRemainingCoins();
+    }
+ 
+    // Method to get current player
+    public int getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    // Method to get remaining coins
+    public int getRemainingCoins() {
+        return coins;
+    }
+}
