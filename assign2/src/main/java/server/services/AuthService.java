45299198065ac6596ea1ee1ae654e3ad9bf49b0f
Mansur <mustafin.mansur02@gmail.com>
2024-05-14 00:40:09 +0100

    finished authUser function

--- assign2/src/main/java/server/services/AuthService.java
+++ assign2/src/main/java/server/services/AuthService.java
@@ -8,24 +8,38 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class AuthService {
-    private Database db;
+    private final Database db;
+    private static final int MAX_ATTEMPTS = 3;
 
     public AuthService(Database db){
         this.db = db;
     }
 
     public User authUser(PrintWriter out, BufferedReader in) throws IOException {
-        String username = "";
-        User user = null;
-        while (true) {
-            out.println("Insert your username:");
+        String username;
+        String password;
+        int attempts = 0;
+
+        while (attempts < MAX_ATTEMPTS) {
+            attempts++;
+
+            out.println("Insert your username:");   // TODO: class 'InputProvider'
             username = in.readLine();
-            user = db.findUserByName(username);
-            if (user == null) out.println("Username doesn't exist");
-            else break;
-            //TODO:password.
+            out.println("Insert your password:");
+            password = in.readLine();
+
+            User user = db.findUserByName(username);
+            if (user == null) {
+                out.println("Username doesn't exist");
+                continue;
+            }
+
+            if (!user.login(password)) {
+                out.println("Login failed: incorrect username or password");
+                continue;
+            }
+            return user;
         }
-        return user;
+        return null;
     }
-    
 }
