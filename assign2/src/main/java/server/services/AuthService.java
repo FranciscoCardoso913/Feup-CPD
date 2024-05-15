337703e8d3066c263d99ffad85a4ab0c246b5cb8
Mansur <mustafin.mansur02@gmail.com>
2024-05-15 15:54:56 +0100

    changed AuthSevice

--- assign2/src/main/java/server/services/AuthService.java
+++ assign2/src/main/java/server/services/AuthService.java
@@ -9,7 +9,6 @@ import java.io.PrintWriter;
 
 public class AuthService {
     private final Database db;
-    private static final int MAX_ATTEMPTS = 3;
 
     public AuthService(Database db){
         this.db = db;
@@ -18,28 +17,23 @@ public class AuthService {
     public User authUser(PrintWriter out, BufferedReader in) throws IOException {
         String username;
         String password;
-        int attempts = 0;
 
-        while (attempts < MAX_ATTEMPTS) {
-            attempts++;
+        out.println("Insert your username:");   // TODO: class 'InputProvider'
+        username = in.readLine();
+        out.println("Insert your password:");
+        password = in.readLine();
 
-            out.println("Insert your username:");   // TODO: class 'InputProvider'
-            username = in.readLine();
-            out.println("Insert your password:");
-            password = in.readLine();
+        User user = db.findUserByName(username);
 
-            User user = db.findUserByName(username);
-            if (user == null) {
-                out.println("Username doesn't exist");
-                continue;
-            }
-
-            if (!user.login(password)) {
-                out.println("Login failed: incorrect username or password");
-                continue;
-            }
-            return user;
+        if (user == null) {
+            out.println("Username doesn't exist");
+            return null;
         }
-        return null;
+
+        if (!user.login(password)) {
+            out.println("Login failed: incorrect username or password");
+            return null;
+        }
+        return user;
     }
 }
